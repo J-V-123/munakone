5bfaba332f28139483d0a9125428ec4b1f5d8e62
J-V-123 <84919828+J-V-123@users.noreply.github.com>
2021-05-27 19:03:40 +0300

    Update demo3.ino

--- demo3.ino
+++ demo3.ino
@@ -1,9 +1,11 @@
+
 #include <OneWire.h>                  
 #include <DallasTemperature.h>        
 #include <Wire.h>                     
 #include <LiquidCrystal_I2C.h>        
 #include <L298N.h>
 #include <HX711.h>    
+#include <math.h>
 
 LiquidCrystal_I2C lcd(0x27, 20,4);  // Set the LCD I2C address
 
@@ -30,9 +32,9 @@ const int motorPin2 = 10;
 
 const int relayPin = 7;
 
-int amount = 0;     // munien määrä
-int doneness = 0;   // keittoaste
-int cooktime;       // keittoaika
+int amount;         // munien määrä
+int doneness;       // keittoaste
+float cooktime;       // keittoaika
 int state = 0;      // ohjelman vaihe
 
 int button1;
@@ -82,19 +84,20 @@ void loop() {
       state = 4;
     }
     else if (state == 4 && temp >= 26)  {
-      menu4();
+			menu4();
       motor(-1);  // laske munat veteen
+      cooktime = cooktime_calc(doneness, amount, mass);
       state = 5;
     }
     else if (state == 5) {
-    bookmarktime = millis()  +  60000 * cooktime;           // set the clock for 5 minutes in the future in milliseconds (1000 x 60 seconds x 6 minutes)
-    state = 6;
+      bookmarktime = millis()  +  60000 * cooktime;           // set the clock for 6 minutes in the future in milliseconds (1000 x 60 seconds x 6 minutes)
+      state = 6;
     }
     else if (state == 6)  {                  
       timeleft = bookmarktime - millis();            // work out the time left on every cycle based on the actual time that's expired according to the Arduino internal clock
-      lcd.setCursor(0,3);
+      lcd.setCursor(0,2);
       lcd.print("Time left:");
-      lcd.setCursor(11,3);
+      lcd.setCursor(11,2);
       lcd.print(timeleft);
       if (timeleft <= 0) {state = 7;}
     }
@@ -110,11 +113,11 @@ void loop() {
 
 int menu1() {   // munien määrän valinta
   lcd.setCursor(0,0);
-  lcd.print("MUNAKONE");
+  lcd.print("------MUNAKONE------");
   lcd.setCursor(0,1);
-  lcd.print("Montako munaa?    ");
+  lcd.print("Montako munaa?      ");
   lcd.setCursor(0,2);
-  lcd.print("1, 2, 3, vai 4?");
+  lcd.print("1, 2, 3, vai 4?     ");
   lcd.setCursor(0,3);
   lcd.print("                    ");
   while(1){
@@ -137,16 +140,16 @@ int menu2()  {    // keittoasteen valinta
   lcd.setCursor(0,1);
   lcd.print("1=loysa, 2=pehmea");
   lcd.setCursor(0,2);
-  lcd.print("3=kova    :DDD");
+  lcd.print("3=kova    :DDD     ");
   lcd.setCursor(0,3);
   lcd.print("                    ");
   while(1){
     button1 = digitalRead(buttonPin1);
     button2 = digitalRead(buttonPin2);
     button3 = digitalRead(buttonPin3);
-    if (button1 == LOW)  {doneness = 1; return doneness;}
-    else if (button2 == LOW)  {doneness = 2; return doneness;}
-    else if (button3 == LOW)  {doneness = 3; return doneness;}
+    if (button1 == LOW)  {doneness = 60; return doneness;}
+    else if (button2 == LOW)  {doneness = 65; return doneness;}
+    else if (button3 == LOW)  {doneness = 70; return doneness;}
   }
 }
 
@@ -201,3 +204,11 @@ void motor(int dir) {     // nosta/laske siivilä
     delay(3000);
   }
 }
+
+float cooktime_calc(int doneness, int amount, float mass)  {   // kaava munien keittoajan laskuun
+  int t_egg = 21;
+  int t_water = 95;  
+  cooktime = 0.451 * power((mass/amount), (2/3)) * log(0.76*((t_egg-t_water)/(doneness-t_water)));
+  retun cooktime;
+}
+
